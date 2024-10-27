d8c31967dfe4bb6b845604c6f8b7bc32598e9526
SmithRH <smith23rh@gmail.com>
2024-10-26 21:51:16 -0500

    second commit - static

--- proyecto_bus/settings.py
+++ proyecto_bus/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-    STATIC_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
